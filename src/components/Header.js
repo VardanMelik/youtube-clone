2e65eedf8c239b3e9384173f4212a01400257b75
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-01-30 00:10:23 +0400

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,25 +5,44 @@ import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import VideoCallIcon from '@material-ui/icons/VideoCall';
 import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MicIcon from '@material-ui/icons/Mic';
+import Avatar from '@material-ui/core/Avatar';
 
 function Header() {
     return (
         <div className="header">
 
-            <IconButton>
-                <MenuIcon/>
-            </IconButton>
+            <div className="header__left">
+                <IconButton>
+                    <MenuIcon/>
+                </IconButton>
 
-            <img
-                className="header__logo"
-                src="./youtube_logo.png"
-                alt="logo"
-            />
-            <input type="text" />
-            <SearchIcon/>
-            <VideoCallIcon />
-            <AppsIcon/>
+                <img
+                    className="header__logo"
+                    src="./youtube_logo.png"
+                    alt="logo"
+                />
+            </div>
+            
 
+            <div className="header__input">
+                <input type="text" placeholder="Search" />
+                <SearchIcon className="header__inputButton"/>
+                <MicIcon/>
+            </div>
+            
+            <div className="header__icons">
+                <VideoCallIcon />
+                <AppsIcon/>
+                <NotificationsIcon/>
+                <Avatar
+                    src="https://avatars.githubusercontent.com/u/49842390"
+                    alt="Avatar"
+                />
+            </div>
+            
+           
         </div>
     )
 }
