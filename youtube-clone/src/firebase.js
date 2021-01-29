f090ed02209b7ae5e2ea9040cc8634a37109a74a
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-01-29 19:43:49 +0400

    upate

---
+++ youtube-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBhnsSvJCmBh51KzTPIDkZMmat9_eyToSI",
+    authDomain: "clone-6d01c.firebaseapp.com",
+    projectId: "clone-6d01c",
+    storageBucket: "clone-6d01c.appspot.com",
+    messagingSenderId: "546589446124",
+    appId: "1:546589446124:web:69c06d613b98b1101efac9"
+  };
